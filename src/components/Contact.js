73fce2ffd2fb4d450c93e8e24f3346293ae30468
wasib <mohammedwasib@gmail.com>
2018-12-31 20:45:00 +0530

    contact-page-done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import "../styles/Contact.css";
 
 export default class Contact extends Component {
   constructor(props) {
@@ -14,8 +14,15 @@ export default class Contact extends Component {
         onClick={this.props.active ? this.props.onClick : null}
       >
         <div className="page-internal">
+          <div className="contact-background" />
+          <div className="contact-noise" />
           <h1>Contact</h1>
-          <p>Hello from the contact page!</p>
+
+          <div className="contact-info">
+            <div className="contact-phone">12345</div>
+            <div className="contact-mail">test@gmail.com</div>
+            <div className="contact-address">Hyderabad, India</div>
+          </div>
         </div>
       </div>
     );
