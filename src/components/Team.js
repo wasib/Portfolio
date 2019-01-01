ac7a86a9fb8724a3ba1cc1ee4a6cc5abef5e30db
wasib <mohammedwasib@gmail.com>
2019-01-01 17:48:43 +0530

    added react reveal to team page

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Fade from "react-reveal/Fade";
 import Background from "./Background";
 import "../styles/Team.css";
 
@@ -18,82 +19,84 @@ export default class Team extends Component {
           <Background />
           <h1>Team</h1>
 
-          <div className="team-container">
-            <div className="team-individual one">
-              <div className="stack">
-                <div className="stack__deco" />
-                <div className="stack__deco" />
-                <div className="stack__deco" />
-                <div className="stack__deco" />
-                <div className="stack__figure">
-                  <img
-                    className="stack__img"
-                    src={require("../images/team-1.png")}
-                    alt="Image"
-                  />
+          <Fade top cascade delay={100}>
+            <div className="team-container">
+              <div className="team-individual one">
+                <div className="stack">
+                  <div className="stack__deco" />
+                  <div className="stack__deco" />
+                  <div className="stack__deco" />
+                  <div className="stack__deco" />
+                  <div className="stack__figure">
+                    <img
+                      className="stack__img"
+                      src={require("../images/team-1.png")}
+                      alt="Image"
+                    />
+                  </div>
+                </div>
+                <div className="team-individual-content">
+                  <div className="team-individual-name">
+                    <h3>Sai Tej Dandge</h3>
+                  </div>
+                  <div className="team-individual-details">
+                    <p>Information about that person here</p>
+                    <p>probably a few lines</p>
+                  </div>
                 </div>
               </div>
-              <div className="team-individual-content">
-                <div className="team-individual-name">
-                  <h3>Sai Tej Dandge</h3>
-                </div>
-                <div className="team-individual-details">
-                  <p>Information about that person here</p>
-                  <p>probably a few lines</p>
-                </div>
-              </div>
-            </div>
 
-            <div className="team-individual two">
-              <div className="stack">
-                <div className="stack__deco" />
-                <div className="stack__deco" />
-                <div className="stack__deco" />
-                <div className="stack__deco" />
-                <div className="stack__figure">
-                  <img
-                    className="stack__img"
-                    src={require("../images/team-2.png")}
-                    alt="Image"
-                  />
+              <div className="team-individual two">
+                <div className="stack">
+                  <div className="stack__deco" />
+                  <div className="stack__deco" />
+                  <div className="stack__deco" />
+                  <div className="stack__deco" />
+                  <div className="stack__figure">
+                    <img
+                      className="stack__img"
+                      src={require("../images/team-2.png")}
+                      alt="Image"
+                    />
+                  </div>
+                </div>
+                <div className="team-individual-content">
+                  <div className="team-individual-name">
+                    <h3>Mohammed Wasib</h3>
+                  </div>
+                  <div className="team-individual-details">
+                    <p>Information about that person here</p>
+                    <p>probably a few lines</p>
+                  </div>
                 </div>
               </div>
-              <div className="team-individual-content">
-                <div className="team-individual-name">
-                  <h3>Mohammed Wasib</h3>
-                </div>
-                <div className="team-individual-details">
-                  <p>Information about that person here</p>
-                  <p>probably a few lines</p>
-                </div>
-              </div>
-            </div>
 
-            <div className="team-individual three">
-              <div className="stack">
-                <div className="stack__deco" />
-                <div className="stack__deco" />
-                <div className="stack__deco" />
-                <div className="stack__deco" />
-                <div className="stack__figure">
-                  <img
-                    className="stack__img"
-                    src={require("../images/team-3.png")}
-                    alt="Image"
-                  />
+              <div className="team-individual three">
+                <div className="stack">
+                  <div className="stack__deco" />
+                  <div className="stack__deco" />
+                  <div className="stack__deco" />
+                  <div className="stack__deco" />
+                  <div className="stack__figure">
+                    <img
+                      className="stack__img"
+                      src={require("../images/team-3.png")}
+                      alt="Image"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="team-individual-content">
-                <div className="team-individual-name">
-                  <h3>Mohammed Aasif</h3>
-                </div>
-                <div className="team-individual-details">
-                  <p>Information about that person here</p>
-                  <p>probably a few lines</p>
+                <div className="team-individual-content">
+                  <div className="team-individual-name">
+                    <h3>Mohammed Aasif</h3>
+                  </div>
+                  <div className="team-individual-details">
+                    <p>Information about that person here</p>
+                    <p>probably a few lines</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </Fade>
         </div>
       </div>
     );
