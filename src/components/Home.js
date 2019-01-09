038bf795fde9fc0c708eb1d6f0872fd3fc442e94
wasib <mohammedwasib@gmail.com>
2019-01-10 01:33:25 +0530

    changed colors

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Background from './Background'
+import Background from "./Background";
+import HomeAnimation from "./HomeAnimation";
+import '../styles/Home.css'
 export default class Home extends Component {
   constructor(props) {
     super(props);
@@ -13,7 +15,9 @@ export default class Home extends Component {
         onClick={this.props.active ? this.props.onClick : null}
       >
         <div className="page-internal">
-        <Background/>
+          <div className="home-animation">
+            <HomeAnimation />
+          </div>
           <h1>Appening</h1>
           <p>Hello from the home page!</p>
           <Link to="/about" className="link link--faded">
