2760efdbe4e54acccc889d519d259b07e51adf58
wasib <mohammedwasib@gmail.com>
2019-01-13 02:41:04 +0530

    fade added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Fade from "react-reveal/Fade";
 import "../styles/Home.css";
 export default class Home extends Component {
   constructor(props) {
@@ -19,10 +20,14 @@ export default class Home extends Component {
             Click here to go to About
           </Link>
           <div className="home-container">
-            <div className="home-content">AAA</div>
-            <div className="home-image">
-              <img src={require("../images/home-image.svg")} />
-            </div>
+            <Fade left>
+              <div className="home-content">AAA</div>
+            </Fade>
+            <Fade right>
+              <div className="home-image">
+                <img src={require("../images/home-image.svg")} />
+              </div>
+            </Fade>
           </div>
         </div>
       </div>
